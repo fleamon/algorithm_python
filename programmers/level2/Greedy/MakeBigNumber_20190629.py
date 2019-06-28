15f1d4480769d1afdfb9e26df859e8ecc926d0c0
fleamon <fleamon@nate.com>
2019-06-29 01:44:06 +0900

    programmers   level2    greedy         MakeBigNumber_20190629

---
+++ programmers/level2/Greedy/MakeBigNumber_20190629.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+# https://programmers.co.kr/learn/courses/30/lessons/42883
+"""
+number	        k	    return
+1924	        2	    94
+1231234	        3	    3234
+4177252841	    4	    775841
+"""
+
+

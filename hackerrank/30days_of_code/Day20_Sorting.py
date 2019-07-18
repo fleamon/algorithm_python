3ea698c1f16f0a761572422c5f31a8c7b48fb411
fleamon <fleamon@nate.com>
2019-07-19 00:51:49 +0900

    hackerrank   30days_of_code   Day20_Sorting, Day22_BinarySearchTree, Day23_BSTLevel_OrderTraversal

---
+++ hackerrank/30days_of_code/Day20_Sorting.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+
+n = int(raw_input().strip())
+a = map(int, raw_input().strip().split(' '))
+# Write Your Code Here
+answer = 0
+for i in range(0, n):
+    swapCnt = 0
+    for j in range(0, n - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+            answer = answer + 1
+            swapCnt = swapCnt + 1
+    if swapCnt == 0:
+        break
+
+print "Array is sorted in " + str(answer) + " swaps."
+print "First Element: " + str(a[0])
+print "Last Element: " + str(a[len(a) - 1])
+
+"""
+3
+1 2 3
+
+
+Array is sorted in 0 swaps.
+First Element: 1
+Last Element: 3
+"""
---
+++ hackerrank/30days_of_code/Day23_BSTLevel-OrderTraversal.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        queue = []
+        if root:
+            queue.insert(0, root)
+            while queue:
+                root = queue.pop()
+                print root.data,
+                if root.left:
+                    queue.insert(0, root.left)
+                if root.right:
+                    queue.insert(0, root.right)
+
+T=int(raw_input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(raw_input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
+
+"""
+6
+3
+5
+4
+7
+2
+1
+
+
+3 2 5 1 4 7
+"""

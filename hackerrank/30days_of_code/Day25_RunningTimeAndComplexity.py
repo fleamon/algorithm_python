f07863eb4c3ff0f1cbb60cf0fe91202237d21036
fleamon <fleamon@nate.com>
2019-08-07 16:06:36 +0900

    hackerrank   30days_of_code   Day25_RunningTimeAndComplexity

---
+++ hackerrank/30days_of_code/Day25_RunningTimeAndComplexity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+n = input()
+for i in range(0, n):
+    chk = input()
+    if chk == 1:
+        print "Not prime"
+    elif chk == 2:
+        print "Prime"
+    else:
+        for i in range(2, int(chk ** 0.5) + 2):
+            if chk % i == 0:
+                print "Not prime"
+                break
+        else:
+            print "Prime"
+
+
+"""
+3
+12
+5
+7
+
+Not prime
+Prime
+Prime
+"""
